def151264961c8116908fe6bc71721cc61021b14
RoshanAditya27 <roshanaditya.m2019@vitstudent.ac.in>
2024-02-06 17:54:22 +0530

    Test Booking repo

--- fastx-busticketbookingsystem/src/main/java/com/hexaware/fastx/repository/BookingRepository.java
+++ fastx-busticketbookingsystem/src/main/java/com/hexaware/fastx/repository/BookingRepository.java
@@ -9,13 +9,9 @@ import com.hexaware.fastx.entities.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 	
-	@Query(
-			  value = "SELECT * FROM booking b where b.userid = ?1", 
-			  nativeQuery = true)
+	@Query(value = "SELECT * FROM booking b where b.userid = ?1", nativeQuery = true)
 	List<Booking> findByUserId(int userId);
 	
-	@Query(
-			  value = "SELECT * FROM booking b where b.scheduleid = ?1", 
-			  nativeQuery = true)
+	@Query(value = "SELECT * FROM booking b where b.scheduleid = ?1", nativeQuery = true)
 	List<Booking> findByScheduleId(int scheduleID);
 }
